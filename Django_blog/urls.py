9118c6c31201c54c1ab2a1cbcada665cba52e610
Adrian Potoniec <adrian.potoniec@gmail.com>
2024-03-19 21:55:30 +0100

    Update files

--- Django_blog/urls.py
+++ Django_blog/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for Django_blog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('posts/', views.posts_list, name='posts_list'),
+    path('posts/<int:pk>/', views.post_details, name='post_details'),
+    path('posts/add/', views.add_post, name='add_post'),
+    path('authors/', views.authors_list, name='authors_list'),
+    path('authors/<int:pk>/', views.author_details, name='author_details'),
+    path('authors/add/', views.add_author, name='add_author'),
 ]
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Author, Post
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('nick', 'email')
+    search_fields = ('nick', 'email')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'content')
+    list_filter = ('author',)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
 # Register your models here.
